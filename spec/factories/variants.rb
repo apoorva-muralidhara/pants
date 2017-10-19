be12d5d0ec28d7ceff89ce5f0f52d09535ed6c78
Apoorva Muralidhara <apoorva@shopkeep.com>
2017-10-19 05:57:45 -0400

    Implemented VariantSerializer, although I'm not using it yet.

---
+++ spec/factories/variants.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :variant do
+    product
+    waist 28
+    length 36
+    style 'stone cutters'
+    count 47
+  end
+end
---
+++ spec/serializers/variant_serializer_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe VariantSerializer do
+  let(:variant) { create(:variant) }
+
+  subject(:serializer_hash) do
+    VariantSerializer.new(variant).serializable_hash
+  end
+
+  it 'includes the waist' do
+    expect(serializer_hash)
+      .to eq(waist: variant.waist,
+             length: variant.length,
+             style: variant.style,
+             count: variant.count)
+  end
+end
