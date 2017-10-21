d756140e268bbaf29aaa56d261e56942ee752cd8
Apoorva Muralidhara <apoorva@shopkeep.com>
2017-10-21 03:05:10 -0400

    Added the missing specs for the included node.

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -12,31 +12,56 @@ RSpec.describe 'Products API', type: :request do
       expect(response).to have_http_status(200)
     end
 
-    it 'returns all the products' do
-      expect(json.size).to eq(3)
+    describe 'data' do
+      subject(:data) { json['data'] }
+
+      it 'returns all the products' do
+        expect(data.size).to eq(3)
+      end
+
+      it 'contains the product ids and types' do
+        expect(data.first).to include('id' => product.id.to_s,
+                                              'type' => 'products')
+      end
+
+      it 'contains the product attributes' do
+        expect(data.first['attributes'])
+          .to eq('name' => product.name,
+                 'description' => product.description,
+                 'image' => product.image)
+      end
+
+      it 'contains the associated variant ids and types' do
+        expect(data.first.dig('relationships', 'variants', 'data'))
+          .to eq([{ 'id' => variant.id.to_s, 'type' => 'variants' }])
+      end
     end
 
-    it 'contains the product ids and types' do
-      expect(json.first).to include('id' => product.id.to_s,
-                                    'type' => 'products')
-    end
+    describe 'included' do
+      subject(:included) { json['included'] }
 
-    it 'contains the product attributes' do
-      expect(json.first['attributes'])
-        .to eq('name' => product.name,
-               'description' => product.description,
-               'image' => product.image)
-    end
+      it 'returns the associated variants' do
+        expect(included.size).to eq(1)
+      end
 
-    it 'contains the id and type of the associated variants' do
-      expect(json.first.dig('relationships', 'variants', 'data'))
-        .to eq([{ 'id' => variant.id.to_s, 'type' => 'variants' }])
+      it 'contains the associated variant ids and types' do
+        expect(included.first).to include('id' => variant.id.to_s,
+                                          'type' => 'variants')
+      end
+
+      it 'contains the associated variant attributes' do
+        expect(included.first['attributes'])
+          .to eq('waist' => variant.waist,
+                 'length' => variant.length,
+                 'style' => variant.style,
+                 'count' => variant.count)
+      end
     end
   end
 
   private
 
   def json
-    JSON.parse(response.body)['data']
+    JSON.parse(response.body)
   end
 end
